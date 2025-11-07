59888a7825560c1a4d53fd53f533be269743c5e3
Manoj Rajput <u133249@straumann.com>
2025-11-07 13:16:22 +0530

    Images fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useScroll, useTransform, motion } from "framer-motion";
 import Nav from "./Nav";
 import { PROFILE } from "../data";
-import Manoj from "../../public/manoj.jpg";
+import Manoj from "../assets/manoj.jpeg";
 
 export default function Header() {
   const { scrollY } = useScroll();
